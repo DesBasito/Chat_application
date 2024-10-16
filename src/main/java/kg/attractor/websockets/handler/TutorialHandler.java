c94a762b989167470105346507a56c996ad63212
DesBasito <116246777+DesBasito@users.noreply.github.com>
2024-10-16 15:21:20 +0600

    checked websocket connection on postman

---
+++ src/main/java/kg/attractor/websockets/handler/TutorialHandler.java
@@ -0,0 +1,39 @@
+package kg.attractor.websockets.handler;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.socket.*;
+
+import java.io.IOException;
+
+@Slf4j
+public class TutorialHandler implements WebSocketHandler {
+
+    @Override
+    public void afterConnectionEstablished(WebSocketSession session) {
+        log.info("Connection established on session: {}", session.getId());
+    }
+
+    @Override
+    public void handleMessage(WebSocketSession session, WebSocketMessage<?> message) throws Exception {
+        String tutorial = (String) message.getPayload();
+        log.info("Message: {}",tutorial);
+        session.sendMessage(new TextMessage("Started processing tutorial: "+session+" - "+tutorial));
+        Thread.sleep(1000);
+        session.sendMessage(new TextMessage("Completed processing tutorial: " + tutorial));
+    }
+
+    @Override
+    public void handleTransportError(WebSocketSession session, Throwable exception) {
+        log.info("Exception occured: {} on session: {}", exception.getMessage(), session.getId());
+    }
+
+    @Override
+    public void afterConnectionClosed(WebSocketSession session, CloseStatus closeStatus) {
+        log.info("Connection closed on session: {} with status: {}", session.getId(), closeStatus.getCode());
+    }
+
+    @Override
+    public boolean supportsPartialMessages() {
+        return false;
+    }
+}
